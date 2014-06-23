18433d404c0ade0b61ea7c0e21782ad754bba700
student <student@PC111S43.fgakusei.local>
2014-06-23 15:26:46 +0900

    6/23

---
+++ src/jp/ac/st/asojuku/original2014002/HitokotoActivity.java
@@ -0,0 +1,7 @@
+package jp.ac.st.asojuku.original2014002;
+
+import android.app.Activity;
+
+public class HitokotoActivity extends Activity {
+
+}
---
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -0,0 +1,20 @@
+package jp.ac.st.asojuku.original2014002;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
