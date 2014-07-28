8c6b7dcbba3e214c6edb7cadfaafeda4cbd2c043
student <student@PC111S43.fgakusei.local>
2014-07-28 15:21:54 +0900

    7/28

--- src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
@@ -2,15 +2,64 @@ package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class MaintenanceActivity extends Activity {
+public class MaintenanceActivity extends Activity implements View.OnClickListener,
+		AdapterView.OnItemClickListener{
 
+	public void setDBValuetoList(ListView listView1){
+		
+		SQLiteCursor cursor = null;
+		
+		if(sdb==null){
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			Log.e("ERROR",e.toString());
+		}
+		cursor = this.helper.selectHitokotoList(sdb);
+		
+		int db_layout = android.R.layout.simple_list_item_activated_1;
+		
+		String[]from = {"phrase"};
+		
+		int[] to = new int[]{android.R.id.text1};
+		
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
+		
+		listView1.setAdapter(adapter);
+	}
+	
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
+	
+	int selectdID = -1;
+	int lastPosition = -1;
+	
 	@Override
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
+		Button btnBack = (Button)findViewById(R.id.btnback);
+		Button btnD = (Button)findViewById(R.id.btnD);
+		ListView listView1 = (ListView)findViewById(R.id.listView1);
+		
+		btnBack.setOnClickListener(this);
+		btnD.setOnClickListener(this);
+		
+		listView1.setOnItemClickListener(this);
+		
+		this.setDBVaiuetoList(listView1);
 	}
 
 	@Override
@@ -27,10 +76,11 @@ public class MaintenanceActivity extends Activity {
 	switch(v.getId()){
 
 	case R.id.btnback:
-		finish();
+		Intent intent = new Intent(MaintenanceActivity.this,MaintenanceActivity.class);
 		
-		break;
+		stratActivity(intent);
 	
 	}
+	
 }
 }
--- src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -90,7 +90,56 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 			//すでにカーソルもcloseしてあるので、何もしない
 		}
 		return rtString;
-
 	}
 
+	/**
+	 * Hitokotoテーブルからのデータをすべて取得
+	 * @param SQLiteDatabase SELECTアクセスするDBインスタンス変数
+	 * @return 取得したデータの塊の表（導入表）のレコードをポイントするカーソル
+	 */
+	public SQLiteCursor selectHitokotoLite(SQLiteDatabase db){
+		
+		SQLiteCursor cursor = null;
+		
+		String sqlstr = " SELECT _id, phrase FROM Hitokoto ORDER BY _id";
+		try{
+			//トランザクション開始
+			cursor =(SQLiteCursor)db.rawQuery(sqlstr, null);
+			if(cursor.getCount()!=0){
+				//カーソル開始位置を先頭にする
+				cursor.moveToFirst();
+			}
+			//cursorは呼び戻しもとへ返すからここではcloseしない
+			//cursor.close();
+		}catch(SQLException e){
+			Log.e("ERROR", e.toString());
+		}finally{
+			
+		}
+	return cursor;
+	
+	}
+	
+	/**
+	 * Hitokoto表から引数(id)で指定した値とカラム[_id」の値が等しいレコードを削除
+	 * @param SQLiteDAtabase DELETEアクセスするDBのインスタンス変数
+	 * @param idカラム[_id]と比較するために指定する削除条件の値
+	 */
+	public void deleteHitokoto(SQLiteDatabase db, int id){
+		
+		String sqlstr = "DELETE FROM Hitokoto where _id = " + id +" ;";
+		try{
+			//トランザクション開始
+			db.beginTransaction();
+			db.execSQL(sqlstr);
+			//トランザクション成功
+			db.setTransactionSuccessful();
+			
+		}catch(SQLException e){
+			Log.e("ERROR", e.toString());
+		}finally{
+			//トランザクション終了
+			db.endTransaction();
+		}
+	}
 }
